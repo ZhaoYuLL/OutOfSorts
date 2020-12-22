7409579328b2ae32ed963c49ef947864c80c02a9
Zhao Yu Lin <>
2020-12-21 23:07:47 -0500

    added insertionsort

--- Sorts.java
+++ Sorts.java
@@ -34,7 +34,15 @@ public class Sorts{
   }
 
  public static void insertionSort(int[] data){
-
+   for(int i = 1; i < data.length; i ++){
+     int j = i;
+     while(j > 0 && data[j-1]>data[j]){
+       int temp = data[j];
+       data[j]=data[j-1];
+       data[j-1] = temp;
+       j -=1;
+     }
+   }
  }
 
 
